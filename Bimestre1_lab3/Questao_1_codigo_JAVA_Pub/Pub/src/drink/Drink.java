b25da7d758109fa7db43d2ea42a69b7d08e2e176
IsabelleFerreira <isabelle.ferreira3000@gmail.com>
2018-09-23 23:06:26 -0300

    minor fixes

--- Bimestre1_lab3/Questao_1_codigo_JAVA_Pub/Pub/src/drink/Drink.java
+++ Bimestre1_lab3/Questao_1_codigo_JAVA_Pub/Pub/src/drink/Drink.java
@@ -7,12 +7,21 @@ import ingredient.Ingredient;
 
 public class Drink {
 	private String name;
-	private int price;
+	private int price = 0;
 	private List<Ingredient> ingredients = new ArrayList<Ingredient>();
 	
 	public Drink(String name, int price, List<Ingredient> ingredients){
 		this.name = name;
-		this.price = price;
+		
+		if(ingredients == null) {
+			this.price = price;
+		}
+		else{
+			for(int i = 0; i < ingredients.size(); i++) {
+				this.price = this.price + ingredients.get(i).getPrice();
+			}
+		}
+		
 		this.ingredients = ingredients;
 	}
 	
