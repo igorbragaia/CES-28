f5d61a5cf7bf08cd423d2d6bf05f82dc543b7fb8
IsabelleFerreira <isabelle.ferreira3000@gmail.com>
2018-09-23 23:06:09 -0300

    constructor of Ingredient

--- Bimestre1_lab3/Questao_1_codigo_JAVA_Pub/Pub/src/ingredient/Ingredient.java
+++ Bimestre1_lab3/Questao_1_codigo_JAVA_Pub/Pub/src/ingredient/Ingredient.java
@@ -4,9 +4,15 @@ public class Ingredient {
 	private String name;
 	private int price;
 	
+	public Ingredient(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
+	
 	public int getPrice() {
 		return this.price;
 	}
